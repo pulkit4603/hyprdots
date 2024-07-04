f8c10e92bc848b7d8596fc253ca34ff4b7f94697
Pulkit Dwivedi <pulkitdwivedi123@gmail.com>
2024-07-05 00:10:09 +0530

    python cf testing added

--- Configs/.config/nvim/lua/mappings.lua
+++ Configs/.config/nvim/lua/mappings.lua
@@ -7,4 +7,8 @@ local map = vim.keymap.set
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
+-- Following is only for testing python code for competitive programming (cf = codeforces)
+-- Changes to be made: make this mapping a function and incorporate c++ testing into it
+map("n", "<leader>cf", "<cmd>!python % < /home/pulkit/cp/input.txt > /home/pulkit/cp/output.txt <CR><CR>")
+
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
