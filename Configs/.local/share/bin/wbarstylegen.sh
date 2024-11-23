3ce1afceb29b19411bcc02686afa98550d2dfae0
Pulkit Dwivedi <pulkitdwivedi123@gmail.com>
2024-11-23 17:57:56 +0530

    changes waybar height

--- Configs/.local/share/bin/wbarstylegen.sh
+++ Configs/.local/share/bin/wbarstylegen.sh
@@ -19,7 +19,7 @@ b_height=`grep '^1|' $conf_ctl | cut -d '|' -f 2`
 
 if [ -z $b_height ] || [ "$b_height" == "0" ]; then
     y_monres=`cat /sys/class/drm/*/modes | head -1 | cut -d 'x' -f 2`
-    b_height=$(( y_monres*3/100 ))
+    b_height=$(( y_monres*7/200 ))
 fi
 
 
